555044e0ae0f4154669576817d487ebe59c03038
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-07 10:38:39 -0300

    Update: understanding about requests and responses

--- server/server.js
+++ server/server.js
@@ -1,8 +1,43 @@
 const http = require("http")
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
 
-    console.log('Request gotten')
+    console.log(req.url, req.method)
+
+    //** Setting response headers */
+    res.setHeader('Content-Type', 'text/html')
+
+    // res.write('<h1>Hello, world!</h1>')
+
+    fs.readFile(`./pages${req.url}.html`, (error, data) => {
+
+        if (error) {
+
+            if(req.url === '/about-me') {
+
+                res.statusCode = 301
+                res.setHeader('Location', '/about')
+                res.end()
+
+                return
+
+            }
+
+            fs.readFile("./pages/404.html", (err, data) => {
+                res.statusCode = 404
+                res.end(data)
+            })
+
+            return
+
+        }
+        
+        // res.write(data)
+        res.statusCode = 200
+        res.end(data)
+
+    })
 
 })
 
