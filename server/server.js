02f93ad5c43d24345ed82f28a74f861842b525ed
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-09 10:08:42 -0300

    Add: exercise about npm

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const http = require("http")
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
 
-    console.log(req.url, req.method)
+    const num = _.random(13, 22)
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log('Hello, world')
+    })
+
+    greet()
+    greet()
 
     //** Setting response headers */
     res.setHeader('Content-Type', 'text/html')
