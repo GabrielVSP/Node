ff39a93a940cb26d15c01cfa6a09be430b98f333
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2025-01-17 09:37:50 -0300

    Add: ejs view engine

--- express/app.js
+++ express/app.js
@@ -2,18 +2,23 @@ const express = require('express')
 
 const app = express()
 
+/**View engine */
+
+app.set('view engine', 'ejs')
+app.set('views', 'pages')
+
 app.listen(3000)
 
 app.get('/', (req, res) => {
 
-    // res.send('<h1>Hello, world!</h1>')
-    res.sendFile('./pages/index.html', { root: __dirname})
+    res.render('index')
 
 })
 
 app.get('/about', (req, res) => {
 
-    res.sendFile('./pages/about.html', { root: __dirname})
+    res.render('about')
+    // res.sendFile('./pages/about.html', { root: __dirname})
 
 })
 
@@ -25,5 +30,5 @@ app.get('/about-me', (req, res) => {
 
 //404 -- Must be at bottom of the file
 app.use((req, res) => {
-    res.status(404).sendFile('./pages/404.html', { root: __dirname})
+    res.status(404).render('404', { root: __dirname})
 })
